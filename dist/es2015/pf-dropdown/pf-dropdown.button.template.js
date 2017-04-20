aedaf22416fc1568b7e7c61fb01e3e52ccd13eed
michpetrov <michpetrov@redhat.com>
2017-04-20 16:04:03 +0000

    Added files generated by Travis build

---
+++ dist/es2015/pf-dropdown/pf-dropdown.button.template.js
@@ -0,0 +1,7 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var pfDropdownButton = "\n  <button class=\"btn btn-primary dropdown-toggle\" type=\"button\"\n    data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n      Dropdown\n    <span class=\"caret\"></span>\n  </button>\n";
+exports.default = pfDropdownButton;
